76ffe4913e7084c6e341a31ec3e56cfde7b14b69
dzaki236 <dzakiahnafz@gmail.com>
2025-06-11 21:30:07 +0700

    change alert padding

--- resources/views/global/alert.blade.php
+++ resources/views/global/alert.blade.php
@@ -1,25 +1,28 @@
-@if ($errors->any())
-    <div class="my-4">
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
+<div class="px-4">
+
+    @if ($errors->any())
+        <div class="my-4">
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
         </div>
-    </div>
-@endif
-@if (Session::has('error'))
-    <div class="my-4">
-        <div class="alert alert-danger" role="alert">
-            <strong>Error!</strong> {{ session('error') }}
+    @endif
+    @if (Session::has('error'))
+        <div class="my-4">
+            <div class="alert alert-danger" role="alert">
+                <strong>Error!</strong> {{ session('error') }}
+            </div>
         </div>
-    </div>
-@endif
-@if (Session::has('success'))
-    <div class="my-4">
-        <div class="alert alert-success" role="alert">
-            <strong>Success!</strong> {{ session('success') }}
+    @endif
+    @if (Session::has('success'))
+        <div class="my-4">
+            <div class="alert alert-success" role="alert">
+                <strong>Success!</strong> {{ session('success') }}
+            </div>
         </div>
-    </div>
-@endif
+    @endif
+</div>
