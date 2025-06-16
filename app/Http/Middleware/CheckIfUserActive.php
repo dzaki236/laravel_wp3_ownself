05ccd2b25ed177ca202983aaa57460f4202fc438
dzaki236 <dzakiahnafz@gmail.com>
2025-06-17 00:38:03 +0700

    check if user not active(fix)

--- app/Http/Middleware/CheckIfUserActive.php
+++ app/Http/Middleware/CheckIfUserActive.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CheckIfUserActive
 {
@@ -18,6 +19,9 @@ class CheckIfUserActive
     {
         if (auth()->check()) {
             if (auth()->user()->status !== 'aktif') {
+                Auth::logout();
+                session()->invalidate();
+                session()->regenerateToken();
                 return redirect()->route('login')->with('error', 'Akun tidak aktif.');
             }
         } else {
